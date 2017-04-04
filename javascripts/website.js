f12df9af4e06c9e22cbb7ee0ab6ef3c5613e31a7
Milena Balerine <mibalerine@gmail.com>
2017-04-04 23:12:07 +0100

    applying diff style for day time

--- javascripts/website.js
+++ javascripts/website.js
@@ -2,3 +2,16 @@ $(window).on("load", function (e) {
 	$('.loader').addClass('fade').css('opacity', 0.0);
 	$('#wrapper').addClass('fade').css('opacity', 1.0);
 })
+
+$(document).ready(function(){
+	var d = new Date();
+	var n = d.getHours();
+	if (n > 18 || n < 6) {
+	  // If time is after 6PM or before 6AM, apply night theme to ‘body’
+	  document.body.className = "night";
+	} else {
+	  // Else use ‘day’ theme
+	  document.body.className = "day";
+	  document.getElementById("bg").src="images/bg-day.jpeg";
+	}
+});
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 var port = process.env.PORT || 9000;
 
+console.log('serving contents of: ', __dirname);
 connect().use(serveStatic(__dirname)).listen(port, function(){
     console.log("Server running on " + port);
 });
